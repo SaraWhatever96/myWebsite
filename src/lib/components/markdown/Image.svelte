5c6d4dab5fe6449695c7e7d007bfd8a835bced39
davide.locatelli <davide.locatelli@accenture.com>
2022-06-28 18:19:03 +0200

    negative margins in markdown images

---
+++ src/lib/components/markdown/Image.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let src;
+  export let alt;
+</script>
+
+<p class="-mx-20">
+  <img {src} {alt}>
+</p>
--- src/lib/components/projects/ProjectsLayout.svelte
+++ src/lib/components/projects/ProjectsLayout.svelte
@@ -1,5 +1,8 @@
+<style global>
+</style>
+
 <div class="max-w-7xl mx-auto pt-20 sm:px-6 lg:px-8">
-  <div class="prose md:prose-lg lg:prose-xl mx-auto dark:prose-invert prose-img:rounded-xl my-16">
+  <div id="article" class="prose md:prose-lg lg:prose-xl mx-auto dark:prose-invert prose-img:rounded-xl my-16">
     <slot />
   </div>
 </div>
