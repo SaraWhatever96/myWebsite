e2de77b59171a9d6775544dbb395ea0120e9d1f3
Davide Locatelli <davidelocatelli@MacBook-Pro-di-Davide.local>
2022-06-28 11:32:22 +0200

    Progress

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -2,10 +2,7 @@ import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
-	layout: {
-		'ui-ux': './src/lib/components/projects/ProjectsLayout.svelte',
-		'motion-design': './src/lib/components/projects/ProjectsLayout.svelte',
-	},
+	layout: './src/lib/components/projects/ProjectsLayout.svelte',
 	smartypants: {
 		dashes: 'oldschool'
 	},
--- src/lib/components/projects/ProjectsLayout.svelte
+++ src/lib/components/projects/ProjectsLayout.svelte
@@ -1,5 +1,5 @@
 <div class="max-w-7xl mx-auto pt-20 sm:px-6 lg:px-8">
-  <div class="prose max-w-none dark:prose-invert">
+  <div class="prose md:prose-lg lg:prose-xl mx-auto dark:prose-invert prose-img:rounded-xl my-16">
     <slot />
   </div>
 </div>
