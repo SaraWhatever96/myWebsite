4cc0fb33995e2bd16c02b691c493f28789e6f8e2
Davide Locatelli <davidelocatelli@MacBook-Pro-di-Davide.local>
2022-09-25 12:42:08 +0200

    align

--- src/lib/components/projects/ProjectsLayout.svelte
+++ src/lib/components/projects/ProjectsLayout.svelte
@@ -6,11 +6,11 @@
 
 <div class="max-w-7xl mx-auto pt-8 md:pt-12 lg:pt-14 px-6 lg:px-8">
   <div id="article"
-  class="prose-lg lg:prose-xl lg:max-w-5xl mx-auto dark:prose-invert prose-slate
+  class="prose-xl lg:prose-xl lg:max-w-5xl mx-auto dark:prose-invert prose-slate
   prose-h1:font-semibold prose-h1:font-title prose-h1:text-slate-700 dark:prose-h1:text-slate-100 prose-h1:text-4xl md:prose-h1:text-4xl lg:prose-h1:text-5xl prose-h1:mb-10
   prose-h2:font-title prose-h2:mt-12 md:prose-h2:mt-24 lg:prose-h2:mt-24 prose-h2:mb-4 lg:prose-h2:mb-6 prose-h2:text-slate-700 dark:prose-h2:text-slate-100 prose-h2:font-semibold
   prose-h4:font-title prose-h4:text-slate-600 dark:prose-h4:text-slate-200
-  prose-p:text-slate-700 dark:prose-p:text-slate-300 prose-p:font-medium
+  prose-p:text-slate-700 dark:prose-p:text-slate-300 prose-p:font-medium prose-li:font-medium prose-li:text-slate-700
   prose-a:text-amber-500 dark:prose-a:text-amber-400">
     <slot />
   </div>
