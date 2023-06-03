e6a76fbbbf6fbd378856344e085ebea62c48b144
davide.locatelli <davide.loca94@gmail.com>
2023-06-03 15:20:24 +0200

    now lottie animations play when they are in view, otherwise they pause

--- src/lib/components/assets/Lottie.svelte
+++ src/lib/components/assets/Lottie.svelte
@@ -2,11 +2,18 @@
 	import { browser } from "$app/environment";
 	import { onMount } from "svelte";
   import lottie from 'lottie-web';
+  import { inview } from 'svelte-inview';
+  import type { ObserverEventDetails, Options } from 'svelte-inview';
 
 
   export let animationName: string;
   let animationContainer: any;
   let lottieAnimation: any;
+  let isInView: boolean;
+  const options: Options = {
+    rootMargin: '50px',
+    unobserveOnEnter: false,
+  };
 
   if (browser) {
     onMount(() => {
@@ -14,13 +21,20 @@
 				container: animationContainer,
 				renderer: 'svg',
 				loop: true,
-				autoplay: true,
+				autoplay: false,
 				path: `/animations/${animationName}.json`
 			});
     })
   }
 </script>
 
-<div bind:this={animationContainer} id="lottiePlayer-{animationName}" class="w-full" />
+<div
+  bind:this={animationContainer}
+  use:inview={options}
+  on:inview_enter={() => lottieAnimation.play()}
+  on:inview_leave={() => lottieAnimation.pause()}
+  id="lottiePlayer-{animationName}"
+  class="w-full"
+/>
 
 
