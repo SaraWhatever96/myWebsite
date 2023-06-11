bbef5e725ffccc8cbee2a853beda731eb2377a70
davide.locatelli <davide.loca94@gmail.com>
2023-06-11 17:31:26 +0200

    having fun with animations

--- src/lib/components/homepage/AboutMeSection.svelte
+++ src/lib/components/homepage/AboutMeSection.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
-	import Lottie from "../assets/Lottie.svelte";
+	import { onMount } from "svelte";
+  import { animate, stagger, inView } from 'motion';
+  import SplitType from 'split-type';
 
+  onMount(() => {
+		inView('p', (info) => {
+      info.target.classList.remove('opacity-0');
+      const paragraph: SplitType = new SplitType(info.target, { type: 'lines' });
+      console.log({paragraph})
+      animate(paragraph.lines, { y: [24, 0], opacity: [0, 1] }, { duration: 0.8, delay: stagger(0.15) });
+		})
+  })
 
 
 </script>
@@ -17,15 +27,15 @@
   <div class="grid grid-cols-1 grid-rows-[1fr_min-content] max-md:gap-y-16 md:grid-cols-2 md:grid-rows-1">
     <!-- First Animation -->
     <div class="inset-shadow max-md:w-full md:h-[min(40vw,35rem)] md:w-[min(40vw,35rem)] rounded-3xl bg-slate-100/70">
-      <Lottie animationName="illu1" />
+      <!-- <Lottie animationName="illu1" /> -->
     </div>
 
     <!-- Description -->
-    <div class="flex flex-col justify-center h-full space-y-5 md:space-y-10">
-      <p class="text-slate-700 font-medium text-lg md:text-xl">
+    <div class="paragraph-container flex flex-col justify-center h-full space-y-5 md:space-y-10">
+      <p class="opacity-0 text-slate-700 font-medium text-lg md:text-xl">
         I am a freelance designer and a startup founder with a bachelor degree in Communication Design through IAAD University.
       </p>
-      <p class="text-slate-700 font-medium text-lg md:text-xl">
+      <p class="opacity-0 text-slate-700 font-medium text-lg md:text-xl">
         Right now I’m diving deep into UI-UX design, visual design and motion design, trying to fullfill my constant desire of learning new skills.
       </p>
     </div>
@@ -35,15 +45,15 @@
   <div class="grid grid-cols-1 grid-rows-[1fr_min-content] max-md:gap-y-16 md:grid-cols-2 md:grid-rows-1">
     <!-- Second Animation -->
     <div class="inset-shadow max-md:w-full md:h-[min(40vw,35rem)] md:w-[min(40vw,35rem)] rounded-3xl bg-slate-100/70 md:col-start-2 md:row-start-1 md:justify-self-end">
-      <Lottie animationName="illu2" />
+      <!-- <Lottie animationName="illu2" /> -->
     </div>
 
     <!-- Description -->
-    <div class="flex flex-col justify-center h-full space-y-5 md:space-y-10 md:col-start-1 md:row-start-1">
-      <p class="text-slate-700 font-medium text-lg md:text-xl">
+    <div class="paragraph-container flex flex-col justify-center h-full space-y-5 md:space-y-10 md:col-start-1 md:row-start-1">
+      <p class="opacity-0 text-slate-700 font-medium text-lg md:text-xl">
         While I'm off work, I find inspiration through my passions. I'm fashinated by Japanese culture and totally into eating with chopsticks.
       </p>
-      <p class="text-slate-700 font-medium text-lg md:text-xl">
+      <p class="opacity-0 text-slate-700 font-medium text-lg md:text-xl">
         I love cinema as it boosts up my powers of observation and helps me learning more about animation. Music is my background during my work and cooking sessions.
       </p>
     </div>
@@ -53,12 +63,12 @@
   <div class="grid grid-cols-1 grid-rows-[1fr_min-content] max-md:gap-y-16 md:grid-cols-2 md:grid-rows-1">
     <!-- Third Animation -->
     <div class="inset-shadow max-md:w-full md:h-[min(40vw,35rem)] md:w-[min(40vw,35rem)] rounded-3xl bg-slate-100/70">
-      <Lottie animationName="illu3" />
+      <!-- <Lottie animationName="illu3" /> -->
     </div>
 
     <!-- Description -->
-    <div class="flex flex-col justify-center h-full space-y-5 md:space-y-10">
-      <p class="text-slate-700 font-medium text-lg md:text-xl">
+    <div class="paragraph-container flex flex-col justify-center h-full space-y-5 md:space-y-10">
+      <p class="opacity-0 text-slate-700 font-medium text-lg md:text-xl">
         These days I'm wondering how an experience in a big company with great designers raising my standards would be like.
       </p>
     </div>
--- src/lib/components/homepage/HeroSection.svelte
+++ src/lib/components/homepage/HeroSection.svelte
@@ -1,28 +1,41 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { animate, stagger } from 'motion';
 	import Waves from '$lib/components/assets/Waves/index.svelte'
 	import SealAnimation from '$lib/components/assets/SealAnimation.svelte';
-	import { onMount } from 'svelte';
 	import type { Coordinates } from '$lib/models/Coordinates';
 	import ClosingWave from '../assets/Waves/ClosingWave.svelte';
+	import SplitType from 'split-type';
+
 
 	let blueSealCoords: Coordinates;
 	let pinkSealCoords: Coordinates;
-	let sealAnimationSize = 112;
+	const sealAnimationSize = 112;
+	const heightSealMobileContainer = 350;
 	let viewportWidth: number;
+	let loaded = false;
 	$: isMobileView = viewportWidth ? viewportWidth < 768 : false;
 
 
 	onMount(() => {
+		loaded = true;
+		animateHeroSection();
+		isMobileView = viewportWidth < 768;
 		initializeSealPosition();
 	});
 
-	function updateSealPositionByColor(event: any) {
-		console.log(event.detail.color);
-		const sealColor: 'blue' | 'pink' = event.detail.color;
-		const { maxColumns, maxRows } = getAvailableArea();
+	function animateHeroSection() {
+		const heroMessage = new SplitType('#hero-message', { types: 'words' });
+		animate(heroMessage.words, { y: [24, 0], opacity: [0, 1] }, { duration: 0.8, delay: stagger(0.10) })
+		animate('.fade-in', { opacity: [0, 1] }, { duration: 1, delay: 0.7, easing: 'ease-in' })
+	}
 
+	function updateSealPositionByColor(event: any) {
+		const sealColor: 'blue' | 'pink' = event.detail.color;
+		const { maxColumns, maxRows, xAxisMargin, yAxisMargin } = getAvailableArea();
 		let mutableCoordinate: Coordinates;
 		let fixedCoordinates: Coordinates;
+
 		if (sealColor === 'blue') {
 			mutableCoordinate = blueSealCoords;
 			fixedCoordinates = pinkSealCoords;
@@ -33,16 +46,10 @@
 
 		do {
       mutableCoordinate = {
-        x: Math.floor(Math.random() * maxColumns) * sealAnimationSize,
-        y: Math.floor(Math.random() * maxRows) * sealAnimationSize
+        x: Math.floor(Math.random() * maxColumns) * sealAnimationSize + xAxisMargin,
+        y: Math.floor(Math.random() * maxRows) * sealAnimationSize + yAxisMargin
       };
     } while (mutableCoordinate.x == fixedCoordinates.x && mutableCoordinate.y == fixedCoordinates.y);
-
-		console.log({mutableCoordinate})
-		if (isMobileView) {
-			const xAxisMargin = Math.round((viewportWidth - (maxColumns * sealAnimationSize)) / 2);
-			mutableCoordinate.x = mutableCoordinate.x + xAxisMargin;
-		}
 
 		if (sealColor === 'blue') {
 			blueSealCoords = mutableCoordinate;
@@ -52,44 +59,38 @@
 	}
 
 	function initializeSealPosition() {
-		const { maxColumns, maxRows } = getAvailableArea();
+		const { maxColumns, maxRows, xAxisMargin, yAxisMargin } = getAvailableArea();
 
 		// Generate random coordinates for the animations, making sure they don't overlap
 		let coordObject1: Coordinates;
 		let coordObject2: Coordinates;
     do {
       coordObject1 = {
-        x: Math.floor(Math.random() * maxColumns) * sealAnimationSize,
-        y: Math.floor(Math.random() * maxRows) * sealAnimationSize
+        x: Math.floor(Math.random() * maxColumns) * sealAnimationSize + xAxisMargin,
+        y: Math.floor(Math.random() * maxRows) * sealAnimationSize + yAxisMargin
       };
       coordObject2 = {
-        x: Math.floor(Math.random() * maxColumns) * sealAnimationSize,
-        y: Math.floor(Math.random() * maxRows) * sealAnimationSize
+        x: Math.floor(Math.random() * maxColumns) * sealAnimationSize + xAxisMargin,
+        y: Math.floor(Math.random() * maxRows) * sealAnimationSize + yAxisMargin
       };
     } while (coordObject1.x == coordObject2.x && coordObject1.y == coordObject2.y);
-
-		if (isMobileView) {
-			const xAxisMargin = Math.round((viewportWidth - (maxColumns * sealAnimationSize)) / 2);
-			coordObject1.x = coordObject1.x + xAxisMargin;
-			coordObject2.x = coordObject2.x + xAxisMargin;
-		}
 
 		blueSealCoords = coordObject1;
 		pinkSealCoords = coordObject2
 	}
 
 	function getAvailableArea() {
-		const halfScreen = window.innerWidth / 2;
+		const halfScreen = viewportWidth / 2;
 		let availableWidth: number;
 		let areaHeight: number;
 
 		if (isMobileView) {
-			areaHeight = 400;
-			availableWidth = window.innerWidth;
+			areaHeight = heightSealMobileContainer;
+			availableWidth = viewportWidth;
 		} else {
 			let waveOffset = 150;
 			areaHeight = 600; // Fixed to 600px for md, lg and xl screen sizes
-			let screenSize = 40 * 16 - waveOffset; // 40 rem * 16px - 150px = 490px
+			let screenSize = 40 * 16 - waveOffset; // 40rem * 16px - 150px = 490px
 			availableWidth = Math.max(250, Math.min(halfScreen, screenSize));
 		}
 
@@ -97,9 +98,15 @@
 		let maxColumns = Math.floor(availableWidth / sealAnimationSize);
 		let maxRows = Math.floor(areaHeight / sealAnimationSize);
 
+		// Mobile Only - Calculate the `x` and `y` margins to center the seals on screen
+		let xAxisMargin = isMobileView ? Math.round((viewportWidth - (maxColumns * sealAnimationSize)) / 2) : 0;
+		let yAxisMargin = isMobileView ? 50 : 0;
+
 		return {
 			maxColumns,
-			maxRows
+			maxRows,
+			xAxisMargin,
+			yAxisMargin
 		};
 	}
 </script>
@@ -111,19 +118,19 @@
 	<div class="m-full-width">
 		<div class="relative inset-shadow bg-slate-100/70 h-[1000px] md:h-[600px] overflow-hidden">
 			<!-- Background under Waves for Desktop -->
-			<div class="absolute z-0 max-md:inset-x-0 max-md:bottom-0 h-1/2 md:inset-y-0 md:w-[45%] md:h-auto md:right-0 bg-water-pink"></div>
+			<div class="fade-in absolute opacity-0 z-0 max-md:inset-x-0 max-md:bottom-0 h-1/2 md:inset-y-0 md:w-[45%] md:h-auto md:right-0 bg-water-pink"></div>
 
 			<!-- Content -->
 			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 z-20 relative">
 				<div class="flex flex-col max-md:space-y-14 md:grid md:grid-cols-2 md:grid-rows-1">
           <div class="max-md:mt-10 md:flex md:flex-col md:justify-center md:h-[600px]">
-            <h1 class="font-semibold text-slate-700 text-5xl leading-[3.5rem]">
+            <h1 id="hero-message" class="{loaded ? 'opacity-100' : 'opacity-0'} font-semibold text-slate-700 text-5xl leading-[3.5rem]">
               May <br />
               design be <br />
               with you <br />
             </h1>
           </div>
-					<div class="relative max-md:m-full-width">
+					<div class="fade-in opacity-0 relative max-md:m-full-width">
 						<!-- Background under Waves for Mobile -->
 						<div class="md:hidden absolute z-0 top-0 inset-x-0 w-auto bg-water-pink"></div>
 						<Waves />
