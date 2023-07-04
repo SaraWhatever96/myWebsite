7ae32c881f44fc8096ae311e6d2e23b3b6af40ee
davide.locatelli <davide.loca94@gmail.com>
2023-07-04 13:08:26 +0200

    added Card component

---
+++ src/lib/components/ui/card/CardFooter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { cn } from "$lib/utils";
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<div class={cn("flex items-center pb-6", className)} {...$$restProps}>
+	<slot />
+</div>
