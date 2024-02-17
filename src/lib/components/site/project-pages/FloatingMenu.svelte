8a6372c2fd2e37506ba399207c36bae8f079a5c5
davide.locatelli <davide.loca94@gmail.com>
2024-02-17 23:06:38 +0100

    Updated the FloatingMenu component UI

--- src/lib/components/site/project-pages/FloatingMenu.svelte
+++ src/lib/components/site/project-pages/FloatingMenu.svelte
@@ -5,22 +5,28 @@
 </script>
 
 
-<div class="sticky top-4 z-50 -mb-[4.5rem] mt-6">
+<div class="sticky top-0 z-50 -mb-8 mt-2 sm:top-4 sm:-mb-[4.5rem] sm:mt-6">
   <div class="w-full flex justify-center">
-    <div class="relative w-full text-[#345556] sm:w-fit p-2.5 rounded backdrop-blur bg-gradient-to-t from-[#BEF2F4]/80 to-background/80 border-2 border-[#345556] supports-backdrop-blur:bg-white/60">
+    <div class="relative w-full text-[#345556] px-5 py-3 sm:w-fit sm:p-2 border-b-2 border-[#345556] sm:rounded backdrop-blur bg-[#BEF2F4]/60 sm:bg-gradient-to-t sm:from-[#BEF2F4]/80 sm:to-background/80 sm:border-2 sm:supports-backdrop-blur:bg-white/60">
       <div class="grid grid-cols-4 gap-x-5">
-        <a href="/dentist-vierin" class="col-span-1 flex font-bold items-center">
-          <Icons.chevronLeft class="w-5 h-5 mr-1" />
-          Prev
-        </a>
-        <a href="https://www.dibarro.com" class="col-span-2 flex font-bold items-center justify-center">
-          <Icons.externalLink class="w-5 h-5 mr-1" />
-          View website
-        </a>
-        <a href="/dentist-vierin" class="col-span-1 flex font-bold items-center">
-          Next
-          <Icons.chevronRight class="w-5 h-5 ml-1" />
-        </a>
+        <div class="col-span-1 justify-self-start">
+          <a href="/dentist-vierin" class="flex font-bold items-center">
+            <Icons.chevronLeft class="w-5 h-5 mr-1" />
+            Prev
+          </a>
+        </div>
+        <div class="col-span-2 justify-self-center">
+          <a href="https://www.dibarro.com" class="flex font-bold items-center justify-center">
+            <Icons.externalLink class="w-5 h-5 mr-1.5" />
+            View website
+          </a>
+        </div>
+        <div class="col-span-1 justify-self-end">
+          <a href="/dentist-vierin" class="flex font-bold items-center ">
+            Next
+            <Icons.chevronRight class="w-5 h-5 ml-1" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
